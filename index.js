af7518b139bf8e4e480ba7ca8b1f832eccf03a92
Davit Jobadze <djobadze8@gmail.com>
2024-05-19 23:10:43 +0400

    refactor styling

---
+++ index.js
@@ -0,0 +1,7 @@
+let message = document.getElementById("error");
+console.log(error);
+
+function change() {
+  console.log("button clicked");
+  error.textContent = "Something went wrong, please try again";
+}
